1accdb8a8c07d2893c2f5c4af0886497fe6b7e56
jack.lowe <jackmlowe@gmail.com>
2019-06-09 00:01:17 +0100

    express hello world

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-const express = require(express);
+const express = require("express");
 
 const app = express();
+
+app.get("/", (req, res) => res.send("Hello world"));
+
+app.listen(8000, () => console.log("listening on port 8000"));
