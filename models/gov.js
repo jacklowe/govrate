1f1d0b09145e7861443bced060ac139b16f79abb
jack.lowe <jackmlowe@gmail.com>
2019-06-10 23:19:23 +0100

    changed columns on sql query to get gov name only

--- models/gov.js
+++ models/gov.js
@@ -2,7 +2,7 @@ const pool = require("../startup/db");
 
 async function getGovs() {
   try {
-    const govs = await pool.query("SELECT * FROM govs");
+    const govs = await pool.query("SELECT country FROM govs");
     return JSON.stringify(govs);
   } catch (err) {
     throw err;
