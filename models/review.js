cdc9edb0eae878eeea3a3470fcc8c3cebd798ec3
jack.lowe <jackmlowe@gmail.com>
2019-06-11 23:20:10 +0100

    added comments outlined plan for api

---
+++ models/review.js
@@ -0,0 +1,9 @@
+// get reviews of particular gov.
+
+// add review
+
+// delete review 
+
+// update review.
+
+// this is full crud i think
---
+++ models/user.js
@@ -0,0 +1,7 @@
+// get users .. no
+
+// add a user
+
+// remove a user (must be authenticated as user)
+
+// get user email and username (admin only)
