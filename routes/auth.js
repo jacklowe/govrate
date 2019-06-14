86b397ad03720ae6518b9139920d6d9c5aa41315
jack.lowe <jackmlowe@gmail.com>
2019-06-14 23:20:01 +0100

    got auth endpoint up so users can log in

--- routes/auth.js
+++ routes/auth.js
@@ -4,15 +4,13 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-  // body is email and password
-  // compare with email in db and look at password...
-  let user = await User.findOne({ email: req.body.email });
+  let user = await findUserByEmail(req.body.email);
   if (!user) return res.status(400).send("Invalid email or password");
 
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) return res.status(400).send("Invalid email or password");
 
-  const token = generateAuthToken();
+  const token = generateAuthToken(userId);
   res.send(token);
 });
 
