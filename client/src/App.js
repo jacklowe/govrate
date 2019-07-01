ef3f640ac4be14152367161f420898ede36c5434
jack.lowe <jackmlowe@gmail.com>
2019-07-01 21:29:15 +0100

    added skeleton of main page.. setup react-router stuff too

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,11 @@ import NavBar from "./components/navBar";
 const App = () => {
   return (
     <React.Fragment>
-      <NavBar user={"jack"} />
+      <NavBar user={""} />
+      <main className="container">
+        <p>govs table</p>
+      </main>
+      <footer>copyright govrate... github linx</footer>
     </React.Fragment>
   );
 };
