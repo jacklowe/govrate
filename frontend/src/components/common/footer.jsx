73bd17df39a80bf5fec2d01e44c74f215fd0cd02
jack.lowe <jackmlowe@gmail.com>
2019-07-03 05:07:01 +0100

    added link to github repo in footer

--- frontend/src/components/common/footer.jsx
+++ frontend/src/components/common/footer.jsx
@@ -3,7 +3,14 @@ import React from "react";
 const Footer = () => {
   const date = new Date();
   const year = date.getFullYear();
-  return <footer>&copy; GovRate {year}</footer>;
+  return (
+    <footer>
+      <p>&copy; GovRate {year}</p>
+      <p>
+        <a href="https://github.com/jacklowe/govrate">github icon</a>
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
