cd78e4b98a67a9736381e91a33de7a189fa8a4f0
jack.lowe <jackmlowe@gmail.com>
2019-07-04 01:56:21 +0100

    nice refactor of arrow function in mapping to get rid of return

--- frontend/src/components/tableBody.jsx
+++ frontend/src/components/tableBody.jsx
@@ -2,21 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const TableBody = ({ govs }) => {
-  const rows = govs.map(gov => {
-    return (
-      <tr key={gov.id}>
-        <td>
-          <Link to="#">{gov.country}</Link>
-        </td>
-        <td>{gov.averageRating}</td>
-        <td>
-          <Link to="/login">
-            <button>Review</button>
-          </Link>
-        </td>
-      </tr>
-    );
-  });
+  const rows = govs.map(gov => (
+    <tr key={gov.id}>
+      <td>
+        <Link to="#">{gov.country}</Link>
+      </td>
+      <td>{gov.averageRating}</td>
+      <td>
+        <Link to="/login">
+          <button>Review</button>
+        </Link>
+      </td>
+    </tr>
+  ));
 
   return <tbody>{rows}</tbody>;
 };
