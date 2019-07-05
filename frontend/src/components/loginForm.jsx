26552cda6294f8dad350ec15c065dc5903a51a3d
jack.lowe <jackmlowe@gmail.com>
2019-07-06 00:57:12 +0100

    simplified form logic

--- frontend/src/components/loginForm.jsx
+++ frontend/src/components/loginForm.jsx
@@ -1,60 +1,50 @@
 import React, { Component } from "react";
 import Input from "./formInput";
 
-// const LoginForm = () => {
-//   return (
-//     <form action="">
-//       <label htmlFor="">
-//         Name:
-//         <input type="text" name="name" />
-//       </label>
-//       <input type="submit" value="submit" />
-//     </form>
-//   );
-// };
-
 class LoginForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { user: {} };
+    this.state = {
+      email: "",
+      password: ""
+    };
 
-    this.handleChange = this.handleChange.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(e) {
-    const user = { ...this.state.data };
-    user[e.currentTarget.name] = e.currentTarget.value;
+  handleEmailChange(e) {
+    this.setState({ ...this.state, email: e.target.value });
+  }
 
-    this.setState({ user });
+  handlePasswordChange(e) {
+    this.setState({ ...this.state, password: e.target.value });
   }
 
   handleSubmit(e) {
-    alert(this.state.user.email + this.state.user.password);
+    alert(this.state.email + " " + this.state.password);
     // do auth stuff...
     e.preventDefault();
   }
 
   render() {
-    const { user } = this.state;
     return (
       <form onSubmit={this.handleSubmit}>
-        <label htmlFor="password">
-          <Input
-            type={"text"}
-            name={"email"}
-            placeholder={"email"}
-            value={user["email"]}
-            onChange={this.handleChange}
-          />
-          <Input
-            type={"password"}
-            name={"password"}
-            placeholder={"Password"}
-            value={user["password"]}
-            onChange={this.handleChange}
-          />
-        </label>
+        <Input
+          type={"text"}
+          name={"email"}
+          placeholder={"email"}
+          value={this.state.email}
+          onChange={this.handleEmailChange}
+        />
+        <Input
+          type={"password"}
+          name={"password"}
+          placeholder={"Password"}
+          value={this.state.password}
+          onChange={this.handlePasswordChange}
+        />
         <input type="submit" value="Log In" />
       </form>
     );
