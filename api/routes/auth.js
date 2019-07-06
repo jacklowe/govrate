dc81c4939c73977934f47f1e428d9b707fd3c94d
Jack Lowe <jackmlowe@gmail.com>
2019-07-06 17:50:14 +0100

    fixed typo

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -13,7 +13,7 @@ router.post("/", async (req, res) => {
 
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword)
-    return res.status(400).send("Invalid email or passwordkk");
+    return res.status(400).send("Invalid email or password");
 
   const token = generateAuthToken(user.userId, user.isAdmin);
   res.send(token);
