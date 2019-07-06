4d3825eb35b2df54be276fd724ac3aabc9b74baa
jack.lowe <jackmlowe@gmail.com>
2019-07-07 00:57:40 +0100

    added some comments outlining design for get request to get gov reviews

--- frontend/src/components/gov.jsx
+++ frontend/src/components/gov.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Gov = props => {
   const id = props.match.params.id;
   // get gov reviews by id
+  // so get request to /api/govs/1/reviews
   return <p>this is the gov component{id}</p>;
 };
 
