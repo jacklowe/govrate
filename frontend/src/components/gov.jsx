b12d8d078d4ed896f25717b3499237e8aa975df9
jack.lowe <jackmlowe@gmail.com>
2019-07-07 01:16:37 +0100

    set up http service with axios

--- frontend/src/components/gov.jsx
+++ frontend/src/components/gov.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import http from "../services/httpService";
 
 const Gov = props => {
   const id = props.match.params.id;
   // get gov reviews by id
   // so get request to /api/govs/1/reviews
+
   return <p>this is the gov component{id}</p>;
 };
 
