da50421e879285b95f3f32184150da5ac6168c0c
Jack Lowe <jackmlowe@gmail.com>
2019-07-07 06:43:20 +0100

    Update paginate.js

--- frontend/src/services/paginate.js
+++ frontend/src/services/paginate.js
@@ -1,14 +1,14 @@
 export default function(data, pageNum, pageLength) {
   const maxPageLength = Math.ceil(data.length / pageLength);
 
-  let page = [];
   const startIndex = (pageNum - 1) * pageLength;
   const endIndex = Math.min(startIndex + pageLength, data.length);
 
   if (pageNum < 1 || pageNum > maxPageLength) {
     return [];
   }
-  
+
+  const page = [];
   for (let i = startIndex; i < endIndex; i++) {
     page.push(data[i]);
   }
