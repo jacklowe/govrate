fee8d8ae42ee2afab212c2fe28e356f485357182
jack.lowe <jackmlowe@gmail.com>
2019-07-09 01:29:44 +0100

    added some simple css to footer

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import "./footer.css";
+
 const Footer = () => {
   const date = new Date();
   const year = date.getFullYear();
   return (
-    <footer>
+    <footer className="Footer">
       <p>&copy; GovRate, {year}.</p>
       <p>
         <a href="https://github.com/jacklowe/govrate">
