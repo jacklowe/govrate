4403cff8415eeb467a73fffe3ccb7f1cfcb24993
jack.lowe <jackmlowe@gmail.com>
2019-07-11 20:50:43 +0100

    added user action types and userActions file

--- frontend/src/redux/actions/types.js
+++ frontend/src/redux/actions/types.js
@@ -1,4 +1,9 @@
+// Math action types
 export const MATH_ADD = "ADD";
 export const MATH_SUBTRACT = "SUBTRACT";
 
+// Gov action types
 export const FETCH_GOV = "FETCH_GOV";
+
+// User action types
+export const FETCH_USER = "FETCH_USER";
