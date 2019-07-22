4dc1a1c6e8bc39d5dbeff1b9018d820f5f954808
jack.lowe <jackmlowe@gmail.com>
2019-07-22 22:38:51 +0100

    changed to better filename

--- api/scripts/seedDatabaseWithCountries.js
+++ api/scripts/seedDatabaseWithCountries.js
@@ -1,2 +1,32 @@
-// seed db with govs data...
-// uses a countries api...
+const axios = require("axios");
+const mysql = require("mysql");
+const config = require("../config");
+
+// initialise environment variables
+require("../startup/config")();
+
+const connection = mysql.createConnection(config.db);
+
+async function main() {
+  const res = await axios.get(
+    "https://restcountries.eu/rest/v2/all?fields=name"
+  );
+  const countries = res.data;
+
+  connection.connect();
+  for (let i = 0; i < countries.length; i++) {
+    connection.query(
+      `INSERT INTO govs (country)
+      VALUES (
+        ?
+      )`,
+      [countries[i].name],
+      function(error, results, fields) {
+        if (error) throw error;
+      }
+    );
+  }
+  connection.end();
+}
+
+main();
